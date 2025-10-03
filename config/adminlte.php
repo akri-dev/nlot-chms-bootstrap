1d164a4d9644b8bc3c35e0bec3160ef2b1c486c3
Akri <alecjoseph.dev@gmail.com>
2025-10-04 01:23:13 +0800

    feat(laravel admin-lte): added branding to left-side navbar

--- config/adminlte.php
+++ config/adminlte.php
@@ -64,11 +64,11 @@ return [
     */
 
     'logo' => 'Church Management',
-    'logo_img' => 'vendor/adminlte/dist/img/nlot-logo.png',
+    'logo_img' => 'img/nlot-logo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
-    'logo_img_xl' => null,
+    'logo_img_xl' => 'img/nlot-logo.png',
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'Admin Logo',
+    'logo_img_alt' => 'Church Management',
 
     /*
     |--------------------------------------------------------------------------
